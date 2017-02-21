bc74b382284b05bd2bc708c3e82fdbc8d04b0436
nicksav <inci@dezcom.org>
2017-02-21 15:02:41 +1000

    restful api get people

--- main.go
+++ main.go
@@ -1,22 +1,59 @@
 package main
 
 import (
+    "net/http"
+    "log"
+    "github.com/gorilla/mux"
 	"encoding/json"
-	"log"
-	"net/http"
-	"strconv"
 )
 
-var count = 1
+type Person struct {
+    ID   string `json:"id, omitempty"`
+    Firstname   string `json:"firstname, omitempty"`
+    Lastname   string `json:"lastname, omitempty"`
+    Address *Address`json:"address, omitempty"`
 
-func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":9010", nil))
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	count++
-	var encoder = json.NewEncoder(w)
-	encoder.Encode(map[string]string{"count": strconv.Itoa(count),
-		"ip": r.RemoteAddr})
+type  Address struct {
+    City string `json:"city, omitempty"`
+    State   string `json:"state, omitempty"`
+
+}
+
+var people []Person
+
+func GetPersonEndPoint (w http.ResponseWriter, req *http.Request){
+    
+}
+
+func GetPeopleEndPoint (w http.ResponseWriter, req *http.Request){
+    
+    json.NewEncoder(w).Encode(people)
+}
+
+func CreatePersonEndPoint (w http.ResponseWriter, req *http.Request){
+    
+}
+
+func DeletePersonEndPoint (w http.ResponseWriter, req *http.Request){
+    
+}
+
+func main() {
+    router := mux.NewRouter()
+    /*Sample Set to define people*/
+    people = append(people, Person{ID: "1", Firstname: "Valeri", Lastname:"Savenko", 
+        Address: &Address{City: "Brisband", State: "QLD"}})
+
+    people = append(people, Person{ID: "2", Firstname: "Nick", Lastname:"Savenko", })
+
+    router.HandleFunc("/people", GetPeopleEndPoint).Methods("GET")
+    router.HandleFunc("/people/{id}", GetPeopleEndPoint).Methods("GET")
+    router.HandleFunc("/people/{id}", CreatePersonEndPoint).Methods("POST")
+    router.HandleFunc("/people/{id}", DeletePersonEndPoint).Methods("DELETE")
+
+
+    log.Fatal(http.ListenAndServe(":12345", router))
+
 }
