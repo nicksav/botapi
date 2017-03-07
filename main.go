54a0575a588a9f0a1df6966869f614af82a660f0
Valeri Savenko <Valeri.Savenko@hastingsdeering.com.au>
2017-03-07 14:41:48 +1000

    test commit ignor

--- main.go
+++ main.go
@@ -2,31 +2,16 @@ package main
 
 import (
 	"fmt"
+	"html"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/webhook", MessengerVerify)
-
-	fmt.Println("Starting server on :8080")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, its ValeBot, %q", html.EscapeString(r.URL.Path))
+	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
-}
 
-func MessengerVerify(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		challenge := r.URL.Query().Get("hub.challenge")
-		verify_token := r.URL.Query().Get("hub.verify_token")
-
-		if len(verify_token) > 0 && len(challenge) > 0 && verify_token == "developers-are-gods" {
-			w.Header().Set("Content-Type", "text/plain")
-			fmt.Fprintf(w, challenge)
-			return
-		}
-	}
-
-	w.Header().Set("Content-Type", "text/plain")
-	w.WriteHeader(400)
-	fmt.Fprintf(w, "Bad Request")
 }
