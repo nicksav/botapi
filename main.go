2a55fec2b44ef52e38e8768d0cb642721036acc0
Valeri Savenko <Valeri.Savenko@hastingsdeering.com.au>
2017-03-07 16:12:11 +1000

    test commit

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		fmt.Fprintf(w, "Hello, its ValeBot %q", html.EscapeString(r.URL.Path))
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
